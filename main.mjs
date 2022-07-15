571f0b1dc5c3abcfbe497f29d0bff3f42926c9c1
LLyma <douglasllyma@hotmail.com>
2022-07-15 09:11:00 -0300

    Primeira Versao

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Douglas', 34));
+console.log(mostraCidade('Douglas', 'São Paulo'));
+console.log(mostraHobby('Douglas', 'Tocar Guitarra'));
